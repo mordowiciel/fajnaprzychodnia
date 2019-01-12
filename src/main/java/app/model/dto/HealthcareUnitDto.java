f67c1cb3c198e176ceb68d71805884e924146e0f
mordowiciel <szymar95@gmail.com>
2019-01-12 21:20:06 +0100

    healthcare unit dto fix

--- src/main/java/app/model/dto/HealthcareUnitDto.java
+++ src/main/java/app/model/dto/HealthcareUnitDto.java
@@ -3,13 +3,17 @@ package app.model.dto;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
 @JsonSerialize
 @JsonDeserialize
+@AllArgsConstructor
+@NoArgsConstructor
 public class HealthcareUnitDto {
     private int id;
     private String unitName;
