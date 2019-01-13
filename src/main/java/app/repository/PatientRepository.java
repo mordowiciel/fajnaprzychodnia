3a350f8706dd79f4f9f5538b3a77a7d4e6662419
mordowiciel <szymar95@gmail.com>
2019-01-13 13:25:39 +0100

    remove redundant query in patient repository

--- src/main/java/app/repository/PatientRepository.java
+++ src/main/java/app/repository/PatientRepository.java
@@ -1,17 +1,12 @@
 package app.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import app.model.entity.Doctor;
 import app.model.entity.Patient;
 import app.model.security.User;
 
 @Repository("patientRepository")
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
     Patient findByUser(User user);
-
-    List<Patient> findByDoctor(Doctor doctor);
 }
